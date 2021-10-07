2f85356584fede76505ffbcf3a6c105589134e90
Syvokobylenko <syvokobylenko@protonmail.com>
2021-10-07 20:09:23 +0200

    added store recognition, you can now check whats in the store

--- get_info.py
+++ get_info.py
@@ -1,2 +1,15 @@
-import cv2 as cv
-import numpy as np
+import pyautogui, time
+
+#top x: 600 y:900
+#bot x: 1350 y: 940
+
+
+def find_champion_shop():
+    import numpy as np 
+
+    champions = ['aatrox', 'gragas', 'kalista', 'khazix', 'kled', 'leona', 'olaf', 'poppy', 'senna', 'udyr', 'vayne', 'vladimir', 'ziggs', 'brand', 'hecarim', 'irelia', 'kennen', 'nautilus', 'pyke', 'sejuani', 'sett', 'soraka', 'syndra', 'thresh', 'tristana', 'varus', 'ashe', 'leesin', 'lulu', 'lux', 'missfortune', 'nidalee', 'nocturne', 'nunu', 'rakan', 'riven', 'yasuo', 'zyra', 'aphelios', 'diana', 'draven', 'fiddlesticks', 'galio', 'ivern', 'jax', 'karma', 'lucian', 'rell', 'velkoz', 'akshan', 'garen', 'gwen', 'heimerdinger', 'kayle', 'teemo', 'viego', 'volibear']
+
+    for champion in champions:
+        location = pyautogui.locateOnScreen(f'screenshots/store/{champion}.png', region=(600,900, 750, 40))
+        if location:
+            print(f"Found {champion}")
